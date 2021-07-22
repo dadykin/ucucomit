bbd9e43c27ecb563ab467cd0758e1c96c6d90e02
dadykin <armostat@mail.ru>
2021-07-22 15:37:09 +0300

    установили gitignore

--- db.php
+++ db.php
@@ -2,10 +2,6 @@
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-require_once 'libs/rb-mysql.php';
-R::setup('mysql:host=localhost;dbname=ucucom','root','root');
-if(!R::testConnection()){
-    exit('NOT CONNECTION WITH BASE');
-}
+require_once "dbconnect.php";
 $i="PEREMENNAY";
 session_start();
